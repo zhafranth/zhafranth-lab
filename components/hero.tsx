7fd648760be0dc66389a0ab302dff026f0ca8a7e
zhafranth <zhafranth@gmail.com>
2025-03-18 21:54:20 +0700

    feat: initial commit

---
+++ components/hero.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Particles from "./react-bits/particles";
+
+const Hero = () => {
+  return (
+    <div className="h-screen relative overflow-hidden">
+      <div style={{ width: "100%", height: "100%", position: "relative" }}>
+        <Particles
+          particleColors={["#ffffff", "#ffffff"]}
+          particleCount={200}
+          particleSpread={20}
+          speed={0.1}
+          particleBaseSize={100}
+          moveParticlesOnHover={true}
+          alphaParticles={false}
+          disableRotation={false}
+        />
+      </div>
+      <div className="absolute top-0 left-0 right-0 bottom-0 w-full h-full z-20 px-[14vw] flex flex-col justify-center">
+        <div className="flex items-end justify-between gap-x-6">
+          <div className="">
+            <p className="text-xl">Hi, I&apos;m Zhafran Tharif</p>
+            <h2 className="text-7xl font-semibold">Software Developer</h2>
+          </div>
+          <div className="">
+            <p className="text-3xl">
+              Transforming ideas into interactive and seamless digital
+              experiences with cutting-edge frontend development.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ components/navbar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import StarBorder from "./react-bits/star-border";
+
+const Navbar = () => {
+  return (
+    <div className="fixed top-0 left-0 right-0 mt-6 flex justify-center z-50">
+      <StarBorder as="button" color="cyan" speed="5s">
+        <a href="#home">Home</a>
+        <a href="#home">Project</a>
+        <a href="#home">Experience</a>
+        <a href="#home">Education</a>
+        <a href="#home">Contact</a>
+      </StarBorder>
+    </div>
+  );
+};
+
+export default Navbar;
