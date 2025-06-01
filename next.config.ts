aaa7bf8f4315818e15ffea6bf402023f2bc5dec4
zhafranth <zhafranth@gmail.com>
2025-06-01 22:03:25 +0700

    fix: remove svgr webpack on next config

--- next.config.ts
+++ next.config.ts
@@ -6,13 +6,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-    return config;
-  },
 };
 
 export default nextConfig;
